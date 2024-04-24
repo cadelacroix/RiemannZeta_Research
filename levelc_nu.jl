9b638926544d11f71e2ddf6625c3f4f21ffd15a6
cadelacroix <cadelacruzm@gmail.com>
2024-04-25 01:45:16 +0300

    Add prints after each stage

--- levelc_nu.jl
+++ levelc_nu.jl
@@ -14,6 +14,7 @@ function main()
     file_content = read("Data/p$(n_dps)/nu_M1-1-200_K1-1-600_s1-1-1_p$(n_dps).json",String)
     nu_str = JSON.parse(file_content)
     nu = Dict(parse_tuple(index) => value for (index,value) in nu_str)
+    println("Opened file.")
     empty!(nu_str)
 
     levelc = Dict{Integer,String}()
@@ -22,10 +23,13 @@ function main()
             levelc[K] = value
         end
     end
+    println("Extracted level curve.")
+
     json_levelc = JSON.json(levelc)
-    open("nu_levelc_Mfloor(K/2)_p$(n_dps).json","w") do f
+    open("Data/p$(n_dps)/nu_levelc_Mfloor(K/2)_p$(n_dps).json","w") do f
         write(f,json_levelc)
     end
+    println("Exported JSON file.")
 end
 
 main()
