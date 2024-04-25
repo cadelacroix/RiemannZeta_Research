b37ed2fdd9f984a69f1536b61372b79142cbd5a7
cadelacruzm zeus <c.delacruz@technion.ac.il>
2024-04-25 07:43:22 +0300

    Remove threads, change exported file name from floor to ceil

--- levelc_nu.jl
+++ levelc_nu.jl
@@ -9,7 +9,7 @@ function parse_tuple(str)
 end
 
 function main()
-    n_dps = 10_000
+    n_dps = 40_000
 
     file_content = read("Data/p$(n_dps)/nu_M1-1-200_K1-1-600_s1-1-1_p$(n_dps).json",String)
     nu_str = JSON.parse(file_content)
@@ -18,7 +18,7 @@ function main()
     empty!(nu_str)
 
     levelc = Dict{Integer,String}()
-    @threads for ((M,K,s),value) in nu 
+    for ((M,K,s),value) in nu 
         if M == Int(ceil(K/2))
             levelc[K] = value
         end
@@ -26,7 +26,7 @@ function main()
     println("Extracted level curve.")
 
     json_levelc = JSON.json(levelc)
-    open("Data/p$(n_dps)/nu_levelc_Mfloor(K/2)_p$(n_dps).json","w") do f
+    open("Data/p$(n_dps)/nu_levelc_MceilKdiv2_p$(n_dps).json","w") do f
         write(f,json_levelc)
     end
     println("Exported JSON file.")
