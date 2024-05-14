58aae0ce73aa675d3337eae03d59f777cac0544b
cadelacruzm zeus <c.delacruz@technion.ac.il>
2024-05-14 16:07:01 +0300

    Open partitioned data

--- levelc_nu.jl
+++ levelc_nu.jl
@@ -9,24 +9,28 @@ function parse_tuple(str)
 end
 
 function main()
-    n_dps = 40_000
-
-    file_content = read("Data/p$(n_dps)/nu_M1-1-200_K1-1-600_s1-1-1_p$(n_dps).json",String)
-    nu_str = JSON.parse(file_content)
-    nu = Dict(parse_tuple(index) => value for (index,value) in nu_str)
-    println("Opened file.")
-    empty!(nu_str)
+    n_dps = 10_000
+    file_content = Dict(i => "" for i in 1:16)
+    nu_str = Dict(i => Dict() for i in 1:16)
+    nu_part = Dict(i => Dict() for i in 1:16)
+    @threads for i in 1:16
+       file_content[i] = read("Data/p$(n_dps)/nu_M1-1-1500_K1-1-3000_s1-1-1_p$(n_dps)_part_$(i).json",String)
+       nu_str[i] = JSON.parse(file_content[i])
+       nu_part[i] = Dict(parse_tuple(index) => value for (index,value) in nu_str[i])
+    end
+    nu = merge(values(nu_part)...)
+    println("Opened files.") 
 
     levelc = Dict{Integer,String}()
     for ((M,K,s),value) in nu 
-        if M == Int(ceil(K/2))
+        if M == K
             levelc[K] = value
         end
     end
     println("Extracted level curve.")
 
     json_levelc = JSON.json(levelc)
-    open("Data/p$(n_dps)/nu_levelc_MceilKdiv2_p$(n_dps).json","w") do f
+    open("Data/p$(n_dps)/nu_levelc_MeqK_p$(n_dps).json","w") do f
         write(f,json_levelc)
     end
     println("Exported JSON file.")
